0c38e29dc8dbd6fa9cd43516166602ad7a3f9d6c
ms27g08 <matthews@silentsentinel.com>
2021-04-15 12:56:58 +0100

    15/04

--- Panoramic/Form1.cs
+++ Panoramic/Form1.cs
@@ -53,43 +53,46 @@ namespace Panoramic
 
       int i = 0;
 
-      //abs pan 0
-      code = new byte[] { 0xFF, 0x01, 0x00, 0x4B, 0x00, 0x00, 0x4C };
-      Peclo_Over_IP.SendCommand(code, IP, Port);
+      ////abs pan 0
+      //code = new byte[] { 0xFF, 0x01, 0x00, 0x4B, 0x00, 0x00, 0x4C };
+      //Peclo_Over_IP.SendCommand(code, IP, Port);
 
-      Thread.Sleep(200);
-      // tilt 0
-      code = new byte[] { 0xFF, 0x01, 0x00, 0x4D, 0x00, 0x00, 0x4E };
-      Peclo_Over_IP.SendCommand(code, IP, Port);  
+      //Thread.Sleep(200);
+      //// tilt 0
+      //code = new byte[] { 0xFF, 0x01, 0x00, 0x4D, 0x00, 0x00, 0x4E };
+      //Peclo_Over_IP.SendCommand(code, IP, Port);  
 
-      Thread.Sleep(3000);
+      //Thread.Sleep(3000);
 
-     // code = new byte[] { 0xFF, 0x01, 0x00, 0x4F, 0xB7, 0x90, 0x97 };
+      //code = new byte[] { 0xFF, 0x01, 0x00, 0x4F, 0xB7, 0x90, 0x97 };
+      //Peclo_Over_IP.SendCommand(code, IP, Port);
+      //Thread.Sleep(5000);
 
-      for (int j = 0; j < 6; j++)
-      {
-        byte[] command;
-        if (6000 + 6000 * j == 36000)
-        {
-          command = new byte[] { 0xFF, 0x01, 0x00, 0x4B, 0x00, 0x00, 0x4C };
-        }
-        else
-        {
-          command = PanAbs(6000 + 6000 * j);
-        }
 
-        Peclo_Over_IP.SendCommand(command, IP, Port);
-        Thread.Sleep(150);
-        Peclo_Over_IP.SendCommand(command, IP, Port);
-        Thread.Sleep(150);
-        Peclo_Over_IP.SendCommand(command, IP, Port);
-        Thread.Sleep(5000);
-        string filename = imagefolder + "\\" + j.ToString() + ".bmp";
+      //for (int j = 0; j < 36; j++)
+      //{
+      //  byte[] command;
+      //  if (1000 + 1000 * j == 36000)
+      //  {
+      //    command = new byte[] { 0xFF, 0x01, 0x00, 0x4B, 0x00, 0x00, 0x4C };
+      //  }
+      //  else
+      //  {
+      //    command = PanAbs(1000 + 1000 * j);
+      //  }
 
-        ImageControl.savesnapshot(ipaddress, username, password, filename, imagefolder, rtsp1);
-        Thread.Sleep(2000);
+      //  Peclo_Over_IP.SendCommand(command, IP, Port);
+      //  Thread.Sleep(150);
+      //  Peclo_Over_IP.SendCommand(command, IP, Port);
+      //  Thread.Sleep(150);
+      //  Peclo_Over_IP.SendCommand(command, IP, Port);
+      //  Thread.Sleep(5000);
+      //  string filename = imagefolder + "\\" + j.ToString() + ".bmp";
 
-      }
+      //  ImageControl.savesnapshot(ipaddress, username, password, filename, imagefolder, rtsp1);
+      //  Thread.Sleep(2000);
+
+      //}
 
       Thread.Sleep(3000);
       string[] fileNames = Directory.GetFiles(imagefolder, "*.bmp");
@@ -106,10 +109,10 @@ namespace Panoramic
 
       Bitmap merged = MergeImages(images);
 
-      merged.Save(imagefolder + "\\Merged.bmp", ImageFormat.Png);
+     // merged.Save(imagefolder + "\\Merged.bmp", ImageFormat.Png);
 
-   //   pictureBox1.Image = merged;
-
+     pictureBox1.Image = merged;
+      pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
 
 
 
@@ -121,6 +124,13 @@ namespace Panoramic
     private static byte[] PanAbs(int panangle)
     {
       string hexValue = panangle.ToString("X");
+
+      if(hexValue.Length == 3)
+      {
+        hexValue = hexValue.Insert(0, "0");
+      }
+
+
       hexValue = hexValue.Insert(2, " ");
 
       string code = "FF 01 00 4B ";
